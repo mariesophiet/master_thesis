0bf013e658eca9411e0faf4be4d94d5a7776b77f
mariesophiet <43850519+mariesophiet@users.noreply.github.com>
2026-01-09 16:28:11 +0100

    Update tls_handshake_test_server.sh
    fixed typo in hybrid server

--- scripts/test_scripts/internal_scripts/tls_handshake_test_server.sh
+++ scripts/test_scripts/internal_scripts/tls_handshake_test_server.sh
@@ -323,7 +323,7 @@ function pqc_tests() {
 
                 elif [ "$test_type" -eq 1 ]; then
                     cert_file="$hybrid_cert_dir/""${sig/:/_}""_srv.crt"
-                    cert_chain="$hybrid_cert_dir/""${sig/:/_}""_int_CA.crt"
+                    cert_chain="$hybrid_cert_dir/""${sig/:/_}""_intCA.crt"
                     key_file="$hybrid_cert_dir/""${sig/:/_}""_srv.key"
                 fi
 
